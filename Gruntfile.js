b5e4a5fb16f82874804fa26d2d4e3e4e4abdf935
John Ghidiu <jghidiu@solutechnology.com>
2014-12-06 22:09:47 -0500

    -Checkpoint for wiredep

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,10 @@ module.exports = function (grunt) {
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
 
-  // Time how long tasks take. Can help when optimizing build times
+    // grunt.loadNpmTasks('grunt-wiredep');
+
+
+    // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
   // Configurable paths
@@ -291,7 +294,27 @@ module.exports = function (grunt) {
           dest: ''
         }]
       }
-    }
+    },
+
+      wiredep: {
+
+          task: {
+
+              // Point to the files that should be updated when
+              // you run `grunt wiredep`
+              src: [
+                  'app/*.html'   // .html support...
+              ],
+
+              options: {
+                  // See wiredep's configuration documentation for the options
+                  // you may pass:
+
+                  // https://github.com/taptapship/wiredep#configuration
+              }
+          }
+      }
+
   });
 
   grunt.registerTask('debug', function () {
